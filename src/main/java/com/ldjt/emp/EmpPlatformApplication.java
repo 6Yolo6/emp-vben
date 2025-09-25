73ca6b56d9342984f3d66abfc132872816623fc2
wudf <xiaobai921104@126.com>
2025-09-25 10:04:02 +0800

    项目初始化

---
+++ src/main/java/com/ldjt/emp/EmpPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ldjt.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ldjt/emp/EmpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldjt.emp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
